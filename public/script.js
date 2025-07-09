3aecd9af89d1ed520876d48670e7bda460d7e265
Toni Noble <thismaybelethal@gmail.com>
2025-07-09 00:05:50 -0400

    better sounds

--- public/script.js
+++ public/script.js
@@ -59,7 +59,7 @@ const instruments = [{
     {
         emoji: '🎷',
         name: 'saxophone',
-        synth: 'FMSynth' // Tone.js FMSynth for sax-like sound
+        synth: 'MonoSynth' // Tone.js FMSynth for sax-like sound
     },
     {
         emoji: '🪕',
@@ -152,16 +152,46 @@ instruments.forEach(instrument => {
             synth = new Tone.AMSynth().toDestination();
             break;
         case 'PolySynth':
-            synth = new Tone.PolySynth(Tone.Synth).toDestination();
+            synth = new Tone.PolySynth(Tone.Synth, {
+				volume: -8,
+				oscillator: {
+					partials: [1, 2, 1],
+				},
+			}).toDestination(); 
             break;
         case 'MembraneSynth':
-            synth = new Tone.MembraneSynth().toDestination();
+            synth = new Tone.MembraneSynth({
+				envelope: {
+					sustain: 0,
+					attack: 0.02,
+					decay: 0.8,
+				},
+				octaves: 10,
+				pitchDecay: 0.01,
+			}).toDestination();
             break;
         case 'FMSynth':
             synth = new Tone.FMSynth().toDestination();
             break;
         case 'PluckSynth':
             synth = new Tone.PluckSynth().toDestination();
+            break;
+        case 'MonoSynth':
+            synth = new Tone.MonoSynth({
+				volume: -10,
+				envelope: {
+					attack: 0.1,
+					decay: 0.3,
+					release: 2,
+				},
+				filterEnvelope: {
+					attack: 0.001,
+					decay: 0.01,
+					sustain: 0.5,
+					baseFrequency: 200,
+					octaves: 2.6,
+				},
+			}).toDestination();
             break;
         case 'Synth':
             synth = new Tone.Synth().toDestination();
